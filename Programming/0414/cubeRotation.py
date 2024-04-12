9b38c139152329f952c15c30899c3727a13fa848
annie <annie.chang@logncoding.com>
2024-04-12 23:23:35 +0900

    cube rotation completed

--- Programming/0414/cubeRotation.py
+++ Programming/0414/cubeRotation.py
@@ -3,88 +3,110 @@ import copy
 colors = ["B", "R", "G", "Y", "O", "P"]
 cube = dict()
 
-input = "B2 CC1 RC4 CR3"
+## Processing input
+input = "G1 CC1 RR5 CR7 RC3"
 inputs = input.split(" ")
+
+### store focus tile and all the moves
 focus, moves = [inputs[0], inputs[1:]]
 
+##Make Cube and store initial position
 def makeCube(cube, input_color, pos):
     for i in range(6):
         cube[i] = []
         color = colors[i]
         if color == input_color: 
+            #if the focus tile is this color, store this face as the starting point 
             initial_pos = (i, (pos // 3, pos % 3))
         for j in range(3): 
             cube[i].append([f"{color}{j*3}", f"{color}{j*3+1}", f"{color}{j*3+2}"])
     return initial_pos
 
 initial_pos = makeCube(cube, focus[0], int(focus[1]))
-print(initial_pos)
 
 def rotate(move, curr_pos):
-    face, (xpos, ypos) = curr_pos
-    direction, rep = move[:2], int(move[2])
-    if(direction[0] == "R"):
-        curr_row = xpos // 3
-        if(face < 4):
-            rotateFaces = [0, 1, 2, 3]
-        elif(face == 4):
-            rotateFaces = [4, 1, 5, 2]
-        else:
-            rotateFaces = [5, 2, 4, 1]
-        curr_pos = rotateRow(curr_pos, curr_row, rotateFaces, direction, rep)
+    face, (curr_row, curr_col) = curr_pos
+    major, direction, rep = move[0], move[1], int(move[2])
+    rotateArray = []
+    
+    ##Store all positions of Rotate Groups
+    if(major == "R"):
+        if(face <= 3):
+            rotateArray =[
+                (0, (curr_row, 0)), (0, (curr_row, 1)), (0, (curr_row, 2)),    
+                (1, (curr_row, 0)), (1, (curr_row, 1)), (1, (curr_row, 2)),    
+                (2, (curr_row, 0)), (2, (curr_row, 1)), (2, (curr_row, 2)),    
+                (3, (curr_row, 0)), (3, (curr_row, 1)), (3, (curr_row, 2)) 
+            ]
+            
+        if(face == 4):
+            rotateArray = [
+                (4, (curr_row, 0)), (4, (curr_row, 1)), (4, (curr_row, 2)),
+                (2, (0, 2-curr_row)), (2, (1, 2-curr_row)), (2, (2, 2-curr_row)),
+                (5, (2-curr_row, 2)), (5, (2-curr_row, 1)), (5, (2-curr_row, 0)),
+                (0, (2, curr_row)), (0, (1, curr_row)), (0, (0, curr_row))
+             ]
+        if(face == 5):
+            rotateArray = [
+                (5, (curr_row, 0)), (5, (curr_row, 1)), (5, (curr_row, 2)),
+                (2, (2, curr_row)), (2, (1, curr_row)), (2, (0, curr_row)),
+                (4, (2-curr_row, 2)), (4, (2-curr_row, 1)), (4, (2-curr_row, 0)),
+                (0, (0, 2-curr_row)), (0, (1, 2-curr_row)), (0, (2, 2-curr_row))
+            ]
+        
+    if(major == "C"):
+        if face in [1, 5, 3, 4]:
+            rotateArray = [
+                (1, (0, curr_col)),  (1, (1, curr_col)), (1, (2, curr_col)),
+                (5, (0, curr_col)),  (5, (1, curr_col)), (5, (2, curr_col)),
+                (3, (2, 2-curr_col)), (3, (1, 2-curr_col)), (3, (0, 2-curr_col)),
+                (4, (0, curr_col)),  (4, (1, curr_col)), (4, (2, curr_col))
+            ]
+        if(face == 0):
+            rotateArray = [
+                (0, (0, curr_col)), (0, (1, curr_col)), (0, (2, curr_col)),
+                (5, (2-curr_col, 0)), (5, (2-curr_col, 1)), (5, (2-curr_col, 2)),
+                (2, (2, 2-curr_col)), (2, (1, 2-curr_col)), (2, (0, 2-curr_col)),
+                (4, (curr_col, 2)), (4, (curr_col, 1)), (4, (curr_col, 0))
+             ]
+        if(face == 2):
+            rotateArray = [
+                (2, (0, curr_col)), (2, (1, curr_col)), (2, (2, curr_col)),
+                (5, (curr_col, 2)), (5, (curr_col, 1)), (5, (curr_col, 0)),
+                (0, (2, 2-curr_col)), (0, (1, 2-curr_col)), (0, (0, 2-curr_col)),
+                (4, (2-curr_col, 0)), (4, (2-curr_col, 1)), (4, (2-curr_col, 2))
+            ]
+    
+    result = rotateMajor(rotateArray, direction, rep)
+    
+    #reassign values to the rotated row or column
+    for i in range(len(rotateArray)):
+        face, (row, col) = rotateArray[i]
+        cube[face][row][col] = result[i]
+        if(focus == result[i]):
+            curr_pos = (face, (row, col))
     return curr_pos
 
-def rotateRow(curr_pos, curr_row, rotateFaces, direction, rep):
-    result = None
-    cube_copy = copy.deepcopy(cube)
-    face, (x, y) = curr_pos
-    if(face <= 3):
-        for i in rotateFaces:
-            for j in range(3):
-                # print("here", i, j, curr_row, rep + j)
-                if direction == "RR":
-                    if(j - rep <= (curr_row + 1) * 3):
-                        shift = (j - rep) // 3
-                        new_face = (i + shift) % 4
-                        new_pos = (j - rep) % 3
-                    else:
-                        new_face = i
-                        new_pos = j - rep
-                else:
-                    if(j + rep >= (curr_row + 1) * 3):
-                        shift = (j + rep) // 3
-                        new_face = (i + shift) % 4
-                        new_pos = (j + rep) % 3
-                    else:
-                        new_face = i
-                        new_pos = j + rep
-                if(curr_pos[0] == new_face and curr_pos[1][1] == new_pos):
-                    result = (i, (curr_row, j))
-                cube[i][curr_row][j] = cube_copy[new_face][curr_row][new_pos]
-        print()
-    elif face == 4:
-        #0th row (4) --> 0th column (0) --> 2nd row (5) --> 2nd column (2)
-        #1st row (4) --> 1st column (0) --> 1st row (5) --> 1st column (2)
-        #2nd row (4) --> 2nd column (0) --> 0th row (5) --> 0th column (2)
-        return
+#rotate the array according to the number of repetitions 
+def rotateMajor(rotateArray, direction, rep):
+    result = []
+    if(direction == "C"):
+        for i in range(len(rotateArray)):
+            face, (row, col) = rotateArray[(i + rep) % len(rotateArray)]
+            result.append(cube[face][row][col])
+    else:
+       for i in range(len(rotateArray)):
+            face, (row, col) = rotateArray[(i - rep) % len(rotateArray)]
+            result.append(cube[face][row][col]) 
     return result
 
-rotate("RR3", initial_pos)
-print(cube)
 
+curr_pos = initial_pos
 
+for move in moves:
+    curr_pos = rotate(move, curr_pos)
 
-
-
-
-
-
-
-"""
-0: RC --> 3 (2), RR --> 1 (0), CC --> 4 (0), CR --> 5 (6)
-1: RC --> 0 (2), RR --> 2 (0), CC --> 4 (6), CR --> 5 (0)
-2: RC --> 1 (2), RR --> 3 (0), CC --> 4 (8), CR --> 5 (5)
-3: RC --> 2 (2), RR --> 0 (0), CC --> 4 (5), CR --> 5 (2)
-4: RC --> 0 (0), RR --> 1 (2), CC --> 5 (6), CR --> 1 (0)
-5: RC --> 1 (8), RR --> 2 (6), CC --> 1 (6), CR --> 4 (0)
-"""
+face = curr_pos[0]
+for row in cube[face]:
+    for square in row:
+        print(square, end = "")
